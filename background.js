3d3ecf18a44e25881964186f997c589e90fe1020
Andriy <khrupalik@gmail.com>
2015-06-29 21:12:00 +0300

    Check if test result exist and waint for response from server.

--- background.js
+++ background.js
@@ -8,65 +8,69 @@ $.get(chrome.extension.getURL('/injected.js'),
         function createJsonResponse() {
             var input;
             var discipline = $(".breadcrumbs-list > li").next().find("a").html();
-            var testId = getCookie("test_id")
+            var testId = getCookie("test_id");
+
+            alert(testId);
+            if (testId !== undefined) {
+                $.get("http://dl.tntu.edu.ua/users/profile.php", function (groupData) {
+                    alert(testId + "   2");
+
+                    var responseArr = [];
+
+                    var group = $(groupData).find("input[id=\"group\"]").attr("value");
+
+                    var elements = $('form[name = \'test\'] fieldset[class=\'group_form\'] > div[class=\'test_instruction\']');
 
 
-if(testId !== undefined)
-            $.get("http://dl.tntu.edu.ua/users/profile.php", function (groupData) {
+                    var responseObject = {};
 
-                var responseArr = [];
+                    responseObject["user"] = $("#header-main-logout-username").text();
+                    responseObject["testId"] = testId;
+                    responseObject["group"] = group;
+                    responseObject["discipline"] = unifyStr(discipline);
+                    responseObject["moduleName"] = unifyStr($("fieldset[class='group_form'] > legend[class='group_form']").text());
+                    alert("Test id: " + testId);
+                    elements.each(function () {
 
-                var group = $(groupData).find("input[id=\"group\"]").attr("value");
+                        if ($("label[for=\"" + $($(this).next().find("input:checked")).attr("id") + "\"]").html() !== undefined) {
 
-                var elements = $('form[name = \'test\'] fieldset[class=\'group_form\'] > div[class=\'test_instruction\']');
+                            var object = {};
 
+                            var scoresOnTest = $(this).find("div").text();
 
-                var responseObject = {};
+                            var questionNumber = $(this).find("h3").text();
 
-                responseObject["user"] = $("#header-main-logout-username").text();
-                responseObject["testId"] = testId
-                responseObject["group"] = group;
-                responseObject["discipline"] = unifyStr(discipline);
-                responseObject["moduleName"] = unifyStr($("fieldset[class='group_form'] > legend[class='group_form']").text());
+                            var question = $(this).next().find("p").text();
 
-                elements.each(function () {
+                            var elements = $(this).next().find("label");
 
-                    if ($("label[for=\"" + $($(this).next().find("input:checked")).attr("id") + "\"]").html() !== undefined) {
+                            var answers = [];
+                            elements.each(function (index) {
+                                answers.push($(this).text().trim());
+                            });
 
-                        var object = {};
-
-                        var scoresOnTest = $(this).find("div").text();
-
-                        var questionNumber = $(this).find("h3").text();
-
-                        var question = $(this).next().find("p").text();
-
-                        var elements = $(this).next().find("label");
-
-                        var answers = [];
-                        elements.each(function (index) {
-                            answers.push($(this).text().trim());
-                        });
-
-                        object["scoreOnQuestion"] = unifyStr(scoresOnTest);
-                        object["questionHeader"] = unifyStr(questionNumber);
-                        object["question"] = unifyStr(question);
-                        object["answers"] = answers;
-                        object["answer"] = unifyStr($("label[for=\"" + $($(this).next().find("input:checked")).attr("id") + "\"]").text());
+                            object["scoreOnQuestion"] = unifyStr(scoresOnTest);
+                            object["questionHeader"] = unifyStr(questionNumber);
+                            object["question"] = unifyStr(question);
+                            object["answers"] = answers;
+                            object["answer"] = unifyStr($("label[for=\"" + $($(this).next().find("input:checked")).attr("id") + "\"]").text());
 
 
-                        responseArr.push(object);
+                            responseArr.push(object);
 
-                    } else if (true) {
+                        }
 
-                    }
+                    });
+
+                    responseObject["body"] = responseArr;
+                    sendResponse(responseObject);
 
                 });
+                for (var i = 0; i < 1000; i++) {
+                    console.log("Wait for get group");
+                }
+            }
 
-                responseObject["body"] = responseArr;
-                sendResponse(responseObject);
-
-            });
         }
 
         var testForm = $("form[name = 'test']")[0];
@@ -84,29 +88,45 @@ if(testId !== undefined)
 
 
         var startTest = $("form[name = 'form']")[0];
+        // alert(startTest);
 
-        if(startTest !== undefined && document.URL.startsWith("http://dl.tntu.edu.ua/mods/_standard/tests/test_intro.php")) {
-            $(startTest).submit(function (event) {
+        /*&& document.URL.startsWith("http://dl.tntu.edu.ua/mods/_standard/tests/test_intro.php")*/
+
+        if (startTest !== undefined) {
+
+            $("form[name = 'form'] input[name = 'submit']").click(function () {
+
                 $.get("http://dl.tntu.edu.ua/users/profile.php", function (groupData) {
-                    
-                    $.ajax({
-                        url: "http://localhost:8080/easytutor/rest/test/temp-test",
-                        type: 'GET',
-                        success: function (data, textStatus) {
-                            setCookie("is_test_submit", "true", {});
-                            setCookie("test_id", data, {});
-                            alert(data);
-                            //$("form[name = 'test']")[0].submit();
-                        },
-                        error: function () {
-                            alert('failure');
-                        }
+
+                    function getTestId() {
+                        return $.ajax({
+                            url: "http://localhost:8080/easytutor/rest/test/temp-test",
+                            type: 'GET',
+                            success: function (data, textStatus) {
+                                setCookie("test_id", data, {expires: 300});
+                                alert(data);
+                                //$("form[name = 'test']")[0].submit();
+                            },
+                            error: function (xhr, status, error) {
+                                var err = eval("(" + xhr.responseText + ")");
+                                alert(err.Message);
+                            }
+
+                        });
+                    }
+
+                    $.when(getTestId()).done(function (a1) {
 
                     });
+
                 });
 
-                
+                var i = 0;
+                for (i = 0; i < 1000; i++) {
+                }
             });
+
+
         }
 
         //$(startTest).click(function () {
@@ -118,24 +138,35 @@ if(testId !== undefined)
 
         function sendResponse(result) {
 
-            $.ajax({
-                url: "http://localhost:8080/easytutor/rest/test/questions",
-                data: JSON.stringify(result),
-                contentType: "application/json",
-                type: 'POST',
-                success: function (data, textStatus) {
-                    // setCookie("is_test_submit", "true", {"expires": 30});
-                    // setCookie("test_id", data, {"expires": 30});
-                    //$("form[name = 'test']")[0].submit();
-                },
-                error: function () {
-                    alert('failure');
-                }
+            function send() {
+                return $.ajax({
+                    url: "http://localhost:8080/easytutor/rest/test/questions",
+                    data: JSON.stringify(result),
+                    contentType: "application/json",
+                    type: 'POST',
+                    success: function () {
+                        alert('Send ok!');
+                        // setCookie("is_test_submit", "true", {"expires": 30});
+                        // setCookie("test_id", data, {"expires": 30});
+                        //$("form[name = 'test']")[0].submit();
+                    },
+                    error: function (xhr, status, error) {
+                        var err = eval("(" + xhr.responseText + ")");
+                        alert(err.Message);
+                    }
 
-            });
+                });
+            }
 
-            for(var i = 0; i < 1000000; i = i+1){} //TODO: Wait for response
+            send();
 
+            for (var i = 0; i < 1000; i++) {
+                console.log("Wait for send to server");
+            }
+            /*$.when(send()).done(function (a1) {
+             alert("Send data to server" + "  " + a1);
+
+             });*/
         }
 
         $("form[name = 'form']").submit(function (e) {
@@ -147,38 +178,41 @@ if(testId !== undefined)
 
         function sendTestResult() {
 
-            var isTestSubmit = getCookie("is_test_submit");
-
-            if (isTestSubmit == "true") {
-
+            var testId = getCookie("test_id");
+            if (testId !== undefined) {
                 var testResult = $('form[method = \'get\'] > div[class=\'input-form\'] > div[class=\'test_instruction\']  span').text().replace("Балів", "").trim();
-                alert(testResult);
-                //TODO: Check if test result exist in page before send to server
-                var result = testResult.split("/")[0];
-                var max = testResult.split("/")[1];
-                var testId = getCookie("test_id");
 
-                var object = {};
-                object["maxScores"] = max.trim();
-                object["scores"] = result.trim();
-                object["id"] = testId;
+                if (testResult !== undefined) {
 
-                console.log(JSON.stringify(object));
-
-                $.ajax({
-                    url: "http://localhost:8080/easytutor/rest/test/scores",
-                    data: JSON.stringify(object),
-                    contentType: "application/json",
-                    type: 'POST',
-                    success: function () {
-                    },
-                    error: function () {
+                    var result = parseInt(testResult.split("/")[0]);
+                    var max = parseInt(testResult.split("/")[1]);
+                    if (result === undefined || max == undefined || testId == undefined || !max || !result) {
+                        return;
                     }
 
-                });
+                    var object = {};
+                    object["maxScores"] = max;
+                    object["scores"] = result;
+                    object["id"] = testId;
 
-                setCookie("is_test_submit", "false", {"expires": 10000});
-                setCookie("test_id", "", {"expires": 10000});
+                    console.log(JSON.stringify(object));
+
+                    $.ajax({
+                        url: "http://localhost:8080/easytutor/rest/test/scores",
+                        data: JSON.stringify(object),
+                        contentType: "application/json",
+                        type: 'POST',
+                        success: function () {
+                        },
+                        error: function () {
+                        }
+
+                    });
+
+                    setCookie("is_test_submit", "false", {"expires": 10000});
+                    //setCookie("test_id", "", {"expires": 10000});
+                }
+
             }
         }
 
--- injected.js
+++ injected.js
@@ -1,7 +1,4 @@
-/**
- * Created by Sergiy on 30.05.2014.
- */
 
 function injected() {
-    console.log("dfdd");
+    console.log("sample");
 }
